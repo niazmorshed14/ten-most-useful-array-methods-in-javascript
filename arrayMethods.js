35c65a089f9f67bd7f0620739357dc126d467f05
Md. Niaz Morshed <niaz.morshed@enosisbd.com>
2025-04-23 03:08:48 +0600

    second commit - removing the line breaks

--- arrayMethods.js
+++ arrayMethods.js
@@ -3,14 +3,12 @@ let lang = ["Java", "JS", "Python"];
 lang.push("Ruby");
 console.log(lang);
 
-console.log("....line break....")
 
 //pop 
 let lang1 = ["Java", "JS", "Python"];
 lang1.pop();
 console.log(lang1);
 
-console.log("....line break....")
 
 //shift -- removes the first element and return the value
 let fruits = ["apple", "banana", "orange", "mango"];
@@ -18,7 +16,6 @@ let firstFruit = fruits.shift();
 console.log(firstFruit);
 console.log(fruits);
 
-console.log("....line break....")
 
 //unshift -- adds one more elements at the begining and returns the length of the array
 let color = ['red', 'green'];
@@ -26,14 +23,12 @@ color.unshift('blue');
 console.log(color); 
 console.log(color.length);
 
-console.log("....line break....")
 
 //splice -- add or remove elements in specific index of an array
 let animals = ['bird', 'cow', 'cat', 'mouse'];
 animals.splice(0,1,'bear');
 console.log(animals);
 
-console.log("....line break....")
 
 //slice -- it's used to extract a portion of an array or string without changing the original one. 
 // it does not include the last index
@@ -42,7 +37,6 @@ let newArray = abc.slice(1,4);
 console.log(newArray);
 
 
-console.log("....line break....")
 
 //concat: combine two or more arrays and create new array
 let fr = ["apple", "banana", "orange", "mango"];
@@ -50,8 +44,6 @@ let nmbr = [1,2,3,4];
 let newConcatArray = fr.concat(nmbr);
 console.log(newConcatArray);
 
-
-console.log("....line break....")
 
 //indexOf: return the first index at which the given element found in the array. return -1 if the element is 
 //not in the array
@@ -63,14 +55,12 @@ console.log(indx);
 let xyz = anmls.indexOf('bird', 1) //start from second element
 console.log(xyz);
 
-console.log("....line break....")
 
 //include -- check whether a single/multiple element is present in the array
 let test = ["admin", "super admin", "owner", "viewer"];
 let flag = test.includes("admin");
 console.log(flag);
 
-console.log("....line break....")
 
 //forEach
 let n = [1,2,3,4,5,6,7];
